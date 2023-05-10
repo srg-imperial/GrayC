b1a6708209abfc076cd8c8fa975eede8e91b1673
Karine EM <karineven79@gmail.com>
2023-05-10 14:02:40 +0100

    Update install-fuzzers-agg.sh

--- ISSTA-2023-AE/fuzzer/install-fuzzers-agg.sh
+++ ISSTA-2023-AE/fuzzer/install-fuzzers-agg.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-base=$1		# /home/user42/git
-llvm=$2		# /home/user42/git/.sources_12VL.9IeP8DH.tmp
-dcovsrc=$3	# /home/user42/git/GrayC-AE
+base=$1		# /home/user42/
+llvm=$2		# /home/user42/.sources_12VL.9IeP8DH.tmp
+dcovsrc=$3	# /home/user42/GrayC/ISSTA-2023-AE
 copies=$4	# 3
-## ./install-fuzzers.sh /home/user42/git /home/user42/git/.sources_12VL.9IeP8DH.tmp /home/user42/git/GrayC-AE 3
+## ./install-fuzzers.sh /home/user42/ /home/user42/.sources_12VL.9IeP8DH.tmp /home/user42/GrayC/ISSTA-2023-AE 3
 
 current_folder=`pwd`
 copy_folder="$base/copy_"
 src="$dcovsrc/AE/tool/src/"
 llvm_local="llvm-csmith-"
 
-echo "base=$1"		# /home/user42/git
-echo "llvm=$2"		# /home/user42/git/.sources_12VL.9IeP8DH.tmp
-echo "dcovsrc=$3"	# /home/user42/git/GrayC-AE
+echo "base=$1"		# /home/user42/
+echo "llvm=$2"		# /home/user42/.sources_12VL.9IeP8DH.tmp
+echo "dcovsrc=$3"	# /home/user42/GrayC/ISSTA-2023-AE
 echo "copies=$4"	# 3
 
 ## Install remove-parens util
@@ -38,7 +38,8 @@ else
 	ninja
 fi
 
-## Install copies of the fuzzer
+## Install copies of the fuzzer - to run parallel GrayC you need to install more than one copy.
+## Note: the default GrayC shall run with a single copy.
 cd $current_folder
 for (( i=1; i<=${copies}; i++ )) ; do
 	c=$copy_folder$i
