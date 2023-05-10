3051c92d0d3e2c141f680dba3ce5e09721658795
Karine EM <karineven79@gmail.com>
2023-05-10 13:59:00 +0100

    Update 3-copy-fuzzer-agg.sh

--- ISSTA-2023-AE/fuzzer/3-copy-fuzzer-agg.sh
+++ ISSTA-2023-AE/fuzzer/3-copy-fuzzer-agg.sh
@@ -56,15 +56,9 @@ cp -r $mutators_code/jump-mutator $clang_tools/
 cp -r $mutators_code/random-manager $clang_tools/
 cp -r $mutators_code/rename-transform $clang_tools/
 
-## Adjust paths
-#sed -i "s:/home/user42/llvm-project:$llvm_folder:g" "$clang_fuzzer_folder/../extract-expression/ExtractExpression.cpp"
-#sed -i "s:/home/user42/llvm-project:$llvm_folder:g" "$clang_fuzzer_folder/../append-expression/AppendExpression.cpp"
-
 ## copy includes for fuzzing
 cp -r $src_folder/../llvm-fuzzer-includes/ $llvm_folder/
 
-## copy settings file
-# cp $src_folder/../settings.txt $working_folder/
 ## Edit the setting file
 echo "mutator_path:$llvm_folder/llvm-fuzzer-build/" > $working_folder/settings.txt
 echo "lib_paths:$llvm_folder/llvm-fuzzer-includes/	/usr/include/x86_64-linux-gnu/	" >> $working_folder/settings.txt
