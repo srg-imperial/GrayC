01c068d9bf85344b21ac763a8ba463f608afdfe6
Karine EM <karineven79@gmail.com>
2023-05-16 21:18:20 +0100

    Create 0-install-grammarinator.sh

---
+++ ISSTA-2023-AE/fuzzer/0-install-grammarinator.sh
@@ -0,0 +1,14 @@
+# You can use this script to install Grammarinator incase the apt is not working
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-11 g++-11
+sudo apt install clang
+sudo apt install git
+sudo apt install python3-pip
+pip3 install gitpython
+sudo apt install python-pip
+python3 -m pip install autopep8
+sudo apt-get install python3-venv
+python3 -m pip install --upgrade pip setuptools wheel
+pip3 install grammarinator
