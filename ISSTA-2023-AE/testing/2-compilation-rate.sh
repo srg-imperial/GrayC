8174f7cf4ec818ded3f06fbb0016203ab67ce911
Karine EM <karineven79@gmail.com>
2023-05-21 16:58:57 +0100

    Create 2-compilation-rate.sh

---
+++ ISSTA-2023-AE/testing/2-compilation-rate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+##################
+## This script check which of the single file programs we can with gcc-11
+## compile given a folder.
+#################
+testcaseDir=$1  #"/home/user42/git/grayc-aggressive/set1"
+seg=$2          # 1
+includes=$3     # "-I/home/user42/git/cov/llvm-csmith-115/csmith/build/runtime/ -I/home/user42/git/cov/llvm-csmith-115/csmith/runtime/"
+
+# Set the counter of passed ones
+y=0
+
+# Check for a segement, how many passing compilation
+for testcaseFile in $testcaseDir/*.segment_"$seg".fuzzer-file-* ; do
+  rm basic_output.txt; echo "yeat" > basic_output.txt
+  grep "yeat" basic_output.txt
+  ( ulimit -t 50 -Sf 90000; gcc-11 -w -O0 $includes $testcaseFile ) >> basic_output.txt 2>&1
+  warnings=`cat basic_output.txt | wc -l`
+  errors=`grep "error:" basic_output.txt | wc -l`
+  linker=`grep "error: linker command failed" basic_output.txt | wc -l`
+  compiler_timeout=`grep "CPU time limit exceeded" basic_output.txt | wc -l`
+  echo "--> Compile	$testcaseFile | $warnings | $errors | $linker | $compiler_timeout"
+
+  if [ $errors -eq 0 ] && [ $linker -eq 0 ] && [ $compiler_timeout -eq 0 ]; then
+    ((y++))
+    #echo ">>>>>>>>>>>>>>>>>>>>>>>> PASSED! $testcaseFile"
+  fi
+  rm basic_output.txt
+  touch basic_output.txt
+done
+
+# statistics
+x=`ls -l $testcaseDir/*.segment_$seg.fuzzer-file-* | wc -l`
+echo ">> Out of $x programs $y pass compilations"
