3643dc643de29b435596d0bd7656af638b8c19ec
Karine <karineven79@gmail.com>
2023-10-23 14:24:43 +0100

    fix the script to work with LLVM 15

--- scripts/coverage/0-install-llvm-cov.sh
+++ scripts/coverage/0-install-llvm-cov.sh
@@ -11,11 +11,12 @@ nb_processes=$3 		## which copy?
 ## Assure we are in gcc-11!
 sudo apt install gcc-11 g++-11
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 1100 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
-sudo rm -f /usr/bin/cpp /usr/bin/gcc /usr/bin/g++ /usr/bin/gcov
-sudo rm -f /usr/local/bin/cpp /usr/local/bin/gcc /usr/local/bin/g++ /usr/local/bin/gcov
+sudo rm -f /usr/bin/c++ /usr/bin/cpp /usr/bin/gcc /usr/bin/g++ /usr/bin/gcov
+sudo rm -f /usr/local/bin/c++ /usr/local/bin/cpp /usr/local/bin/gcc /usr/local/bin/g++ /usr/local/bin/gcov
 sudo ln -s /usr/bin/cpp-11 /usr/bin/cpp
 sudo ln -s /usr/bin/gcc-11 /usr/bin/gcc
 sudo ln -s /usr/bin/g++-11 /usr/bin/g++
+sudo ln -s /usr/bin/g++-11 /usr/bin/c++
 sudo ln -s /usr/bin/gcov-11 /usr/bin/gcov
 echo ">> End Settings"
 
@@ -48,11 +49,13 @@ cpp-11 --version >> $working_folder/llvm-csmith-$i/compilation_info/llvm-version
 /usr/bin/cc --version >> $working_folder/llvm-csmith-$i/compilation_info/llvm-version.txt
 /usr/bin/c++ --version >> $working_folder/llvm-csmith-$i/compilation_info/llvm-version.txt
 
+mv llvm-project llvm-source
 ### LLVM PART: with instrumentation
 # Setting the env. + cov.
 {
 	mkdir llvm-build llvm-install
 	cd ./llvm-build
+
 	set CFLAGS='--coverage -ftest-coverage -fprofile-arcs -fno-inline'
 	set CXXFLAGS='--coverage -ftest-coverage -fprofile-arcs -fno-inline'
 	set LDFLAGS='-lgcov --coverage -ftest-coverage -fprofile-arcs'
@@ -61,10 +64,12 @@ cpp-11 --version >> $working_folder/llvm-csmith-$i/compilation_info/llvm-version
 
 	# Cmake and build of LLVM
 	timeS=$(date +"%T")
-	echo "Configuration: cmake -GNinja ../llvm-source -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DLLVM_BUILD_DOCS=OFF -DCMAKE_C_FLAGS=--coverage -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage <$timeS>" >> $working_folder/llvm-csmith-$i/compilation_info/llvm-version.txt
-	cmake -GNinja ../llvm-source -DCMAKE_BUILD_TYPE="Release" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_BUILD_EXAMPLES="OFF" -DLLVM_BUILD_TESTS="OFF" -DLLVM_BUILD_DOCS="OFF" -DCMAKE_C_FLAGS="--coverage" -DCMAKE_CXX_FLAGS="--coverage" -DCMAKE_EXE_LINKER_FLAGS="--coverage" > $working_folder/llvm-csmith-$i/compilation_info/config_output.txt 2>&1
+	echo "Configuration: cmake -G Ninja -Wall ../llvm-source/llvm/ -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_DOCS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DLLVM_BUILD_DOCS=OFF -DCMAKE_C_FLAGS=--coverage -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_EXE_LINKER_FLAGS=--coverage -Wno-dev <$timeS>" >> $working_folder/llvm-csmith-$i/compilation_info/llvm-version.txt
+	cmake -G Ninja -Wall ../llvm-source/llvm/ -DLLVM_ENABLE_PROJECTS='clang' -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_INCLUDE_TESTS="OFF" -DLLVM_INCLUDE_DOCS="OFF" -DLLVM_INCLUDE_BENCHMARKS="OFF" -DLLVM_BUILD_EXAMPLES="OFF" -DLLVM_BUILD_TESTS="OFF" -DLLVM_BUILD_DOCS="OFF" -DCMAKE_C_FLAGS="--coverage" -DCMAKE_CXX_FLAGS="--coverage" -DCMAKE_EXE_LINKER_FLAGS="--coverage" -Wno-dev > $working_folder/llvm-csmith-$i/compilation_info/config_output.txt 2>&1
+
 	timeB=$(date +"%T")
 	echo ">> Build LLVM with ninja to ./llvm-build <$timeB>"
+
 	ninja > $working_folder/llvm-csmith-$i/compilation_info/build_output.txt 2>&1
 	ninja check-clang >> $working_folder/llvm-csmith-$i/compilation_info/build_output.txt 2>&1
 	grep "FAILED:" $working_folder/llvm-csmith-$i/compilation_info/build_output.txt
